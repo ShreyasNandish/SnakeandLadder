06ef1944cf14698c8dd64f449e03fbb2c3c790b0
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-09 14:59:48 +0530

    Modified-added the program with initial position at position 0

--- src/com/bridgelabz/SnakeandLadder/SnakeandLadder.java
+++ src/com/bridgelabz/SnakeandLadder/SnakeandLadder.java
@@ -1,5 +1,5 @@
 package com.bridgelabz.SnakeandLadder;
-
+import java.util.Scanner;
 public class SnakeandLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
