e541409c2d43d8517a9d34c0b577ff82c494e02c
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-09 14:54:56 +0530

    added Snake and ladder game program

---
+++ src/com/bridgelabz/SnakeandLadder/SnakeandLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.SnakeandLadder;
+
+public class SnakeandLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Game");
+
+        int currentPosition = 0;
+    }
+}
