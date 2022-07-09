e00eafe320de65b65d18d2817e142cd3090235db
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-09 15:06:53 +0530

    Modified:added the snake and ladder with rolling of dice

--- src/com/bridgelabz/SnakeandLadder/SnakeandLadder.java
+++ src/com/bridgelabz/SnakeandLadder/SnakeandLadder.java
@@ -5,5 +5,7 @@ public class SnakeandLadder {
         System.out.println("Welcome to Snake And Ladder Game");
 
         int currentPosition = 0;
+        int diceNumber = (int) ((Math.random() * 10 % 6) + 1);
+        System.out.println("Dice Number is : " + diceNumber);
     }
 }
