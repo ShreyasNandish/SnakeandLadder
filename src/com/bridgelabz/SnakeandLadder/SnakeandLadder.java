a583fda4238e893cf57c81567b401707d07c421a
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-13 14:19:24 +0530

    Modified: modified the code in order to get the exact winning position

--- src/com/bridgelabz/SnakeandLadder/SnakeandLadder.java
+++ src/com/bridgelabz/SnakeandLadder/SnakeandLadder.java
@@ -5,25 +5,44 @@ public class SnakeandLadder {
     public static final int START_POSITION = 0;
     public static final int IS_SNAKE = 1;
     public static final int IS_LADDER = 2;
+    public static final int NO_PLAY = 0;
+    public static int currentPosition = 0;
+
+    public static void playerPositionMoves() {
+        while (currentPosition < 100) {
+            int diceNumber = (int) ((Math.random() * 10 % 6) + 1);
+            System.out.println("Dice Number : " + diceNumber);
+
+            int option = (int) ((Math.random() * 10 % 3) + 1);
+            if (option == IS_SNAKE) {
+                currentPosition -= diceNumber;
+                System.out.println("Snake is bites....");
+            }
+            else if (option == IS_LADDER) {
+                currentPosition += diceNumber;
+            }
+            else if (option == NO_PLAY) {
+                System.out.println("NO Play");
+            }
+
+            if (currentPosition < START_POSITION) {
+                currentPosition = START_POSITION;
+            } else if (currentPosition > 100) {
+                currentPosition -= diceNumber;
+            }
+            System.out.println("Player moves position: " + currentPosition);
+        }
+    }
+
+    public static void playUntilWin() {
+        while (currentPosition != 100) {
+            playerPositionMoves();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
-
-        int currentPosition = 0;
-        int diceNumber = (int) ((Math.random() * 10 % 6) + 1);
-        System.out.println("Dice Number is : " + diceNumber);
-
-        int option = (int) ((Math.random() * 10 % 3) + 1);
-        if (option == IS_SNAKE) {
-            currentPosition -= diceNumber;
-            if(currentPosition<0){
-                currentPosition=START_POSITION;
-            }
-            System.out.println("Player got with snake position : " + currentPosition);
-        } else if (option == IS_LADDER) {
-            currentPosition += diceNumber;
-            System.out.println("Player got ladder position : " + currentPosition);
-        } else {
-            System.out.println("No Play " + currentPosition);
-        }
+        playUntilWin();
+        System.out.println("Player won the game");
     }
 }
